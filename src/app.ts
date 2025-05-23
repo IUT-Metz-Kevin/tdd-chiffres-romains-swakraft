72d71661c1431a095ef97918b2a3fbb6738bb450
Hugo Beets <hugo.beets2003@gmail.com>
2025-05-23 10:46:47 +0200

    test passed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 export function romanNumeral(value: number): string {
     let result = "";
-    
+    while (value >= 1000) {
+        result += "M";
+        value -= 1000;
+    }
 
     return result
 }
