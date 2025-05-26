26845a0d65d4b359975943907492d5ce17d3cc32
Hugo Beets <hugo.beets2003@gmail.com>
2025-05-26 16:11:54 +0200

    test passed

--- src/app.ts
+++ src/app.ts
@@ -58,9 +58,13 @@ export function RomanToNumeral(value: string): number {
     let result = 0;
     const romanNum: { [key: string]: number } = {
         "M": 1000,
+        "CM": 900,
         "D": 500,
+        "CD": 400,
         "C": 100,
+        "XC": 90,
         "L": 50,
+        "XL": 40,
         "X": 10,
         "IX": 9,
         "V": 5,
