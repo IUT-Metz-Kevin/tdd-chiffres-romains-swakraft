a1b2dcd08c51be1dbbe515165db15a455fcd0c19
Hugo Beets <hugo.beets2003@gmail.com>
2025-05-26 14:57:49 +0200

    added tests

--- tests/num.test.ts
+++ tests/num.test.ts
@@ -4,3 +4,8 @@ import { RomanToNumeral } from "../src/app.ts";
 Deno.test("cas avec 1", () => {
     assertEquals(RomanToNumeral("I"), 1);
 });
+
+Deno.test("cas avec 2 et 3", () => {
+    assertEquals(RomanToNumeral("II"), 2);
+    assertEquals(RomanToNumeral("III"), 3);
+});
